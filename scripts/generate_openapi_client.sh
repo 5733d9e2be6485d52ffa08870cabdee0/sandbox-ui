d35a00bd3762efe2a09cadba4928c79b4865d455
roberto emanuel <kelvah@gmail.com>
2022-03-22 11:29:38 +0100

    [MGDOBR-420] Generate TS SDK from OpenApi spec (#6)
    * MGDOBR-420: add scripts and settings
    
    * MGDOBR-420: add generated api client
    
    * MGDOBR-420: add instructions to README.md
    
    * MGDOBR-420: add --yes to override confirmation prompt during client generation
    
    * Make generate script runable from anywhere (#1)
    
    * Make generate script runable from anywhere
    
    * Remove 'ignore-file-override' due to default file location
    
    Co-authored-by: Jozef Marko <jomarko@redhat.com>

---
+++ scripts/generate_openapi_client.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+openapi_filename=$(dirname $0)/../openapi/smart-events.yaml
+output_path=$(dirname $0)/../openapi/generated
+package_name="smart-events"
+additional_properties="ngVersion=6.1.7,npmName=${package_name},supportsES6=true,withInterfaces=true,withSeparateModelsAndApi=true,modelPackage=model,apiPackage=api"
+
+npx @openapitools/openapi-generator-cli version-manager set 5.4.0
+echo "Generating SDKs"
+
+# validate the openapi spec for smart-events
+
+echo "Validating OpenAPI ${openapi_filename}"
+npx @openapitools/openapi-generator-cli validate -i "$openapi_filename"
+
+echo "Clearing existing client"
+
+# remove old generated models
+
+rm -Rf $output_path/model $output_path/api
+
+# generate an API client for smart-events
+
+echo "Generating source code based on ${openapi_filename}"
+
+npx --yes @openapitools/openapi-generator-cli generate -g typescript-axios -i \
+"$openapi_filename" -o "$output_path" \
+--package-name=$package_name \
+--additional-properties=$additional_properties

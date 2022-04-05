a42dbb80910f7221ba98ecc52fe1074cc7d29e2d
vpellegrino <vpellegr@redhat.com>
2022-04-05 11:51:58 +0200

    Fixing types with global.d.ts

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -1,18 +1,20 @@
 // Declare global variables for TypeScript and VSCode.
 // Do not rename this file or move these types into index.d.ts
 // @see https://code.visualstudio.com/docs/nodejs/working-with-javascript#_global-variables-and-type-checking
+// noinspection JSUnusedGlobalSymbols
+
 declare const __DEV__: boolean;
 declare const __VERSION__: string;
-declare const $FixMe: any;
+declare const $FixMe: never;
 declare module "*.svg" {
-  const content: any;
+  const content: never;
   export default content;
 }
 declare module "*.json" {
-  const value: any;
+  const value: never;
   export default value;
 }
 
 declare function __webpack_init_sharing__(s: string): Promise<void>;
-declare const __webpack_share_scopes__: { [key: string]: any };
+declare const __webpack_share_scopes__: { [key: string]: never };
 declare const __webpack_public_path__: string;

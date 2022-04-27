bd0395837072469a287c0bf3098c5eebb56e5f71
roberto emanuel <kelvah@gmail.com>
2022-04-27 14:30:22 +0200

    MGDOBR-591: API mocks for SE instances (#25)
    * MGDOBR-590: add msw setup and some demo handlers
    
    * MGDOBR-590: updates following rebase onto main
    
    * MGDOBR-590: move msw stuff to dedicated folder
    
    * MGDOBR-591: add handlers for SE instances mocks
    
    * MGDOBR-591: add .env with APIs config

--- mocked-api/data.ts
+++ mocked-api/data.ts
@@ -1,6 +1,4 @@
-import { BridgeResponse } from "../openapi/generated";
-
-export const instancesData: BridgeResponse[] = [
+export const instancesData = [
   {
     kind: "Bridge",
     id: "3543edaa-1851-4ad7-96be-ebde7d20d717",
@@ -8,7 +6,7 @@ export const instancesData: BridgeResponse[] = [
     href: "/api/v1/bridges/3543edaa-1851-4ad7-96be-ebde7d20d717",
     submitted_at: "2022-04-12T12:04:43.044590+0000",
     published_at: "2022-04-12T12:06:22.881959+0000",
-    status: "READY",
+    status: "ready",
     endpoint:
       "https://ob-3543edaa-1851-4ad7-96be-ebde7d20d717.apps.openbridge-dev.fdvn.p1.openshiftapps.com/events",
   },
@@ -19,7 +17,7 @@ export const instancesData: BridgeResponse[] = [
     href: "/api/v1/bridges/3af03c9c-ba8a-11ec-8422-0242ac120002",
     submitted_at: "2022-04-12T12:36:43.044590+0000",
     published_at: "2022-04-12T12:38:22.881959+0000",
-    status: "READY",
+    status: "ready",
     endpoint:
       "https://ob-3af03c9c-ba8a-11ec-8422-0242ac120002.apps.openbridge-dev.fdvn.p1.openshiftapps.com/events",
   },

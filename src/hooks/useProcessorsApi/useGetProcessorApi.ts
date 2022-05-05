8105fc25c56611707afd16d9cc506909fd122384
roberto emanuel <kelvah@gmail.com>
2022-05-05 18:37:02 +0200

    updates following peer review

--- src/hooks/useProcessorsApi/useGetProcessorApi.ts
+++ src/hooks/useProcessorsApi/useGetProcessorApi.ts
@@ -4,12 +4,11 @@ import {
   ProcessorsApi,
 } from "@openapi/generated";
 import { useCallback, useState } from "react";
+import { useAuth } from "@rhoas/app-services-ui-shared";
+import config from "../../../config/config";
 
-export function useGetProcessorApi(
-  getToken: () => Promise<string>,
-  basePath: string
-): {
-  getProcessor: (bridgeId: string, processorId: string) => Promise<void>;
+export function useGetProcessorApi(): {
+  getProcessor: (bridgeId: string, processorId: string) => void;
   processor?: ProcessorResponse;
   isLoading: boolean;
   error: unknown;
@@ -17,22 +16,27 @@ export function useGetProcessorApi(
   const [processor, setProcessor] = useState<ProcessorResponse>();
   const [error, setError] = useState<unknown>();
   const [isLoading, setIsLoading] = useState(true);
+  const auth = useAuth();
+
+  const getToken = useCallback(async (): Promise<string> => {
+    return (await auth.kas.getToken()) || "";
+  }, [auth]);
 
   const getProcessor = useCallback(
-    async (bridgeId: string, processorId: string): Promise<void> => {
+    (bridgeId: string, processorId: string) => {
       const processorsApi = new ProcessorsApi(
         new Configuration({
           accessToken: getToken,
-          basePath,
+          basePath: config.apiBasePath,
         })
       );
-      await processorsApi
+      processorsApi
         .getProcessor(bridgeId, processorId)
         .then((response) => setProcessor(response.data))
         .catch((err) => setError(err))
         .finally(() => setIsLoading(false));
     },
-    [getToken, basePath]
+    [getToken]
   );
 
   return { getProcessor, isLoading, processor, error };

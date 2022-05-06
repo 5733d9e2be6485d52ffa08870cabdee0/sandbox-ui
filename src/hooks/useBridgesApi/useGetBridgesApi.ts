b317fd472d38a053064562ded6e87d04e249fd42
vpellegrino <vpellegr@redhat.com>
2022-05-06 15:31:45 +0200

    Fix useGetBridgesApi to export bridgeListResponse

--- src/hooks/useBridgesApi/useGetBridgesApi.ts
+++ src/hooks/useBridgesApi/useGetBridgesApi.ts
@@ -1,22 +1,20 @@
-import { BridgesApi, Configuration, BridgeResponse } from "@openapi/generated";
+import {
+  BridgeListResponse,
+  BridgesApi,
+  Configuration,
+} from "@openapi/generated";
 import { useCallback, useState } from "react";
 import { useAuth } from "@rhoas/app-services-ui-shared";
 import config from "config/config";
-import {
-  DEFAULT_PAGE_SIZE,
-  FIRST_PAGE,
-} from "@app/components/TableWithPagination/TableWithPagination";
 
 export function useGetBridgesApi(): {
-  getBridges: (
-    pageReq?: number,
-    sizeReq?: number
-  ) => Promise<void | { page: number; size: number; total: number }>;
-  bridges?: BridgeResponse[];
+  getBridges: (pageReq?: number, sizeReq?: number) => void;
+  bridgeListResponse?: BridgeListResponse;
   isLoading: boolean;
   error: unknown;
 } {
-  const [bridges, setBridges] = useState<BridgeResponse[]>();
+  const [bridgeListResponse, setBridgeListResponse] =
+    useState<BridgeListResponse>();
   const [error, setError] = useState<unknown>();
   const [isLoading, setIsLoading] = useState(true);
   const auth = useAuth();
@@ -26,32 +24,22 @@ export function useGetBridgesApi(): {
   }, [auth]);
 
   const getBridges = useCallback(
-    (
-      pageReq?: number,
-      sizeReq?: number
-    ): Promise<void | { page: number; size: number; total: number }> => {
+    (pageReq?: number, sizeReq?: number): void => {
+      setIsLoading(true);
       const bridgeApi = new BridgesApi(
         new Configuration({
           accessToken: getToken,
           basePath: config.apiBasePath,
         })
       );
-      return bridgeApi
+      bridgeApi
         .getBridges(pageReq, sizeReq)
-        .then((response) => {
-          const data = response.data;
-          setBridges(data.items);
-          return {
-            page: data.page ?? FIRST_PAGE,
-            size: data.size ?? DEFAULT_PAGE_SIZE,
-            total: data.total ?? 0,
-          };
-        })
+        .then((response) => setBridgeListResponse(response.data))
         .catch((err) => setError(err))
         .finally(() => setIsLoading(false));
     },
     [getToken]
   );
 
-  return { getBridges, isLoading, bridges, error };
+  return { getBridges, isLoading, bridgeListResponse, error };
 }

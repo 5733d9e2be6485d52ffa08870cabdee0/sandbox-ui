212d7ba34ea932a350e58c3b4c2bdcfea8a6eff1
Jozef Marko <jomarko@redhat.com>
2022-09-19 14:04:48 +0200

    Migration from cypress 9 to 10 (#184)

--- cypress/plugins/index.ts
+++
@@ -1,7 +0,0 @@
-/**
- * @type {Cypress.PluginConfig}
- */
-export default (on, config) => {
-  // `on` is used to hook into various events Cypress emits
-  // `config` is the resolved Cypress config
-};

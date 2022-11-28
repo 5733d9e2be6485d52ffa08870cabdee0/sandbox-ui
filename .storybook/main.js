5db729d830bc03e5d792de0f0168ec2a4ea97286
roberto emanuel <kelvah@gmail.com>
2022-11-28 17:53:07 +0100

    improve storybook config, add example stories

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,5 @@
+const TsconfigPathsPlugin = require("tsconfig-paths-webpack-plugin");
+
 module.exports = {
   stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: [
@@ -5,6 +7,15 @@ module.exports = {
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
   ],
+  webpackFinal: async (config) => {
+    config.resolve.plugins = [
+      ...(config.resolve.plugins || []),
+      new TsconfigPathsPlugin({
+        extensions: config.resolve.extensions,
+      }),
+    ];
+    return config;
+  },
   framework: "@storybook/react",
   core: {
     builder: "@storybook/builder-webpack5",
---
+++ src/app/components/SEStatusLabel/SEStatusLabel.stories.tsx
@@ -0,0 +1,64 @@
+import React from "react";
+
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+
+import SEStatusLabel from "./SEStatusLabel";
+import { ManagedResourceStatus } from "@rhoas/smart-events-management-sdk";
+
+export default {
+  title: "Shared/SEStatusLabel",
+  component: SEStatusLabel,
+  args: {
+    requestedAt: new Date(),
+  },
+} as ComponentMeta<typeof SEStatusLabel>;
+
+const Template: ComponentStory<typeof SEStatusLabel> = (args, { viewMode }) => {
+  const inDocs = viewMode === "docs";
+
+  return (
+    <div style={{ paddingTop: inDocs ? 0 : 250 }}>
+      <SEStatusLabel {...args} />
+    </div>
+  );
+};
+
+export const Accepted = Template.bind({});
+Accepted.args = {
+  status: ManagedResourceStatus.Accepted,
+  resourceType: "bridge",
+};
+
+export const Preparing = Template.bind({});
+Preparing.args = {
+  status: ManagedResourceStatus.Preparing,
+  resourceType: "bridge",
+};
+
+export const Provisioning = Template.bind({});
+Provisioning.args = {
+  status: ManagedResourceStatus.Provisioning,
+  resourceType: "bridge",
+};
+
+export const Ready = Template.bind({});
+Ready.args = {
+  status: ManagedResourceStatus.Ready,
+  resourceType: "bridge",
+};
+
+export const Deprovision = Template.bind({});
+Deprovision.args = {
+  status: ManagedResourceStatus.Deprovision,
+  resourceType: "bridge",
+};
+
+export const Deleting = Template.bind({});
+Deleting.args = {
+  status: ManagedResourceStatus.Deleting,
+};
+
+export const Deleted = Template.bind({});
+Deleted.args = {
+  status: ManagedResourceStatus.Deleted,
+};

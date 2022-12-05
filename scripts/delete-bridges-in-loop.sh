8c9b81f9ff0df17fc843da7f5a8b1a9596b79d1b
Sona Mala <smala@redhat.com>
2022-12-05 22:51:22 +0100

    MGDOBR-1034: create a new script from CI bash source code

---
+++ scripts/delete-bridges-in-loop.sh
@@ -0,0 +1,11 @@
+for r in 1 2 3 4
+do
+    echo "Run script #$r:"
+    ./scripts/delete-bridges.sh
+    if [ $? -eq 0 ]; then
+        echo "Clean rutine exits after $r performation(s)"
+        exit 0
+    else
+        sleep 90
+    fi
+done

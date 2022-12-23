e848b15d0412f65b154bec9d7a0a61e1be3e40d6
roberto emanuel <remanuel@redhat.com>
2022-12-23 16:16:44 +0100

    MGDOBR-1328: Add existing components stories to storybook (#283)
    * add stories for existing components
    
    * remove commented code
    
    * updates following rebase

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,7 +2,11 @@ const TsconfigPathsPlugin = require("tsconfig-paths-webpack-plugin");
 const MonacoWebpackPlugin = require("monaco-editor-webpack-plugin");
 
 module.exports = {
-  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  stories: [
+    "../docs/**/*.stories.mdx",
+    "../src/**/*.stories.mdx",
+    "../src/**/*.stories.@(js|jsx|ts|tsx)",
+  ],
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",

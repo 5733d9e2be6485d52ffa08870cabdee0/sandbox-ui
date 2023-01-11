1763853603a758d355edfea4845c4360a1007b9a
vpellegrino <vpellegr@redhat.com>
2023-01-11 17:11:33 +0100

    Create ProcessorTemplateSelector

--- src/app/components/POCs/ProcessorTemplateSelector/ProcessorTemplateSelector.stories.tsx
+++ src/app/components/POCs/ProcessorTemplateSelector/ProcessorTemplateSelector.stories.tsx
@@ -2,15 +2,65 @@ import React from "react";
 
 import { ComponentMeta, ComponentStory } from "@storybook/react";
 import { ProcessorTemplateSelector } from "@app/components/POCs/ProcessorTemplateSelector/ProcessorTemplateSelector";
+import { PlusCircleIcon } from "@patternfly/react-icons";
 
 export default {
   title: "PoCs/Processor Template Selector",
   component: ProcessorTemplateSelector,
 } as ComponentMeta<typeof ProcessorTemplateSelector>;
 
-const Template: ComponentStory<typeof ProcessorTemplateSelector> = () => (
-  <ProcessorTemplateSelector />
+const Template: ComponentStory<typeof ProcessorTemplateSelector> = (args) => (
+  <ProcessorTemplateSelector {...args} />
 );
 
-export const Test = Template.bind({});
-Test.args = {};
+export const TemplateSelection = Template.bind({});
+
+TemplateSelection.args = {
+  templates: [
+    {
+      icon: PlusCircleIcon,
+      title: "Basic Processor",
+      description:
+        "This basic processor template has a filter and a transformation template.",
+      code: `- kind: Processor
+metadata:
+  name: processor-name
+  bridge_id: a8c1cb57-0ab3-4ccb-8c55-ef8a8b1646
+flows:
+- from:
+    uri: "rhose:bridge"
+    steps:
+    - filter:
+        simple: '\${header.type} == "StorageService"'
+        steps:
+        - kamelet:
+            name: template
+            parameters:
+              template: '{"text": "hello {body.name}"}'
+        - to:
+            uri: "sink-name"`,
+    },
+    {
+      icon: PlusCircleIcon,
+      title: "Routing Processor",
+      description:
+        "This routing processor template has context based routing. It will send events to different sinks based on some conditions.",
+      code: `- kind: Processor
+metadata:
+  name: processor-name
+  bridge_id: a8c1cb57-0ab3-4ccb-8c55-ef8a8b1646
+flows:
+- from:
+    uri: rhose:bridge
+    steps:
+    - choice:
+        when:
+          - simple: "\${body.nutritions.sugar} <= 5"
+            to:
+              uri: "SlackAction1"
+          - simple: "\${body.nutritions.sugar} > 5 && \${body.nutritions.sugar} <= 10"
+            to:
+              uri: "HttpAction1"`,
+    },
+  ],
+};
